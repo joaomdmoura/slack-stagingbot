561ec6a83e0b231e554d96b8bc4a4923281e9bde
João M. D. Moura <joaomdmoura@gmail.com>
2018-02-27 22:46:34 -0300

    rescuing to avoind silly errors

--- stagingbot.rb
+++ stagingbot.rb
@@ -2,11 +2,11 @@ require 'yaml/store'
 require 'slack-ruby-bot'
 
 class Database
-  attr_accessor :file
   DATABASE = YAML::Store.new('stagingbot_db.yml')
 
   def self.staging(number)
-    file["staging_usage"][number.to_i]
+    database = file
+    database["staging_usage"][number.to_i]
   end
 
   def self.save_staging_usage(number, user)
@@ -15,34 +15,34 @@ class Database
       DATABASE["staging_usage"][number.to_i] = user
     end
   end
-
-  def file
-    @file ||= YAML.load_file('stagingbot_db.yml')
-  end
 end
 
 class StagingBot < SlackRubyBot::Bot
   command /use staging [1-4]/ do |client, data, match|
-    staging_number = /[1-4]/.match(match['command']).to_s
-    user = Database.staging(staging_number)
+    begin
+      staging_number = /[1-4]/.match(match['command']).to_s
+      user = Database.staging(staging_number)
 
-    if user
-      client.say(text: "I'm sorry <@#{data.user}>, but staging #{staging_number} is reserved to <@#{user}>.", channel: data.channel)
-    else
-      Database.save_staging_usage(staging_number, data.user)
-      client.say(text: "Okay <@#{data.user}>, staging #{staging_number} is reserved to you.", channel: data.channel)
+      if user
+        client.say(text: "I'm sorry <@#{data.user}>, but staging #{staging_number} is reserved to <@#{user}>.", channel: data.channel)
+      else
+        Database.save_staging_usage(staging_number, data.user)
+        client.say(text: "Okay <@#{data.user}>, staging #{staging_number} is reserved to you.", channel: data.channel)
+      end
     end
   end
 
   command /release staging [1-4]/ do |client, data, match|
-    staging_number = /[1-4]/.match(match['command']).to_s
-    user = Database.staging(staging_number)
+    begin
+      staging_number = /[1-4]/.match(match['command']).to_s
+      user = Database.staging(staging_number)
 
-    if user != data.user
-      client.say(text: "I'm sorry <@#{data.user}>, you can't release staging #{staging_number} because <@#{user}> is the one that reserved it.", channel: data.channel)
-    else
-      Database.save_staging_usage(staging_number, nil)
-      client.say(text: "Okay <@#{data.user}>, staging #{staging_number} is released.", channel: data.channel)
+      if user != data.user
+        client.say(text: "I'm sorry <@#{data.user}>, you can't release staging #{staging_number} because <@#{user}> is the one that reserved it.", channel: data.channel)
+      else
+        Database.save_staging_usage(staging_number, nil)
+        client.say(text: "Okay <@#{data.user}>, staging #{staging_number} is released.", channel: data.channel)
+      end
     end
   end
 
